c1a632056b91b2b335afb12ce82d3501b5582697
Piotrek <slysz.piotrek@gmail.com>
2019-01-09 21:56:10 +0100

    Komunikaty debugowe

--- connection.h
+++ connection.h
@@ -20,7 +20,7 @@
 class Connection
 {
   public:
-    static const int READ_SIZE = 256;
+    static const int READ_SIZE = 256; 
     static const int REQUEST_SIZE_LIMIT = 10000; // maximum length of a single request
     using string = std::string;
   private:
@@ -41,7 +41,7 @@ class Connection
         this->read_fdset = nullptr;
         this->write_fdset = nullptr;
     }
-    Connection(int socket, User *user = nullptr, fd_set* rfdset=nullptr, fd_set* wfdset=nullptr):
+    Connection(int socket, User *user = nullptr, fd_set* rfdset=nullptr, fd_set* wfdset=nullptr): 
     requests(),responses(),recived_chars()
     {
         this->socket = socket;
@@ -58,24 +58,24 @@ class Connection
     User* getUser() const {return user;}
     void setUser(User* user ){this->user = user;}
     int getSocket() const{return socket;}
-    void setSocket(int socket){this->socket = socket;}
+    void setSocket(int socket){this->socket = socket;} 
     //return request from Q fron without popping it
     string getRequest() const {return requests.front();}
     // Pops request from the queue and reurns it
     string popRequest()
     {
-        string req = requests.front();
+        string req = requests.front(); 
         requests.pop_front();
         return req;
     }
     string getResponse() const { return responses.front();}
-    void setResponse(string res)
+    void setResponse(string res) 
     {
         responses.push_back(res);
     }
     bool isRequsetComplete() const {return (requests.size() > 0);}
     bool responsePending() const {return (responses.size() > 0);}
-
+        
 
     // Read data from socket and return read result
     int reciveMsg()
@@ -98,24 +98,26 @@ class Connection
                 else
                 {//'\0' means that wohle request has been recived - push it in the Q and reset recived_chars
                     requests.push_back(string(recived_chars.begin(), recived_chars.end()));
+                    std::cout<<"["<<socket<<"]NEW Request:"<<requests.back()<<std::endl<<std::flush;
                     recived_chars.clear();
                 }
-            }
+            }    
         }
         return rval;
     }
 
     void sendResponse()
-    {
+    {   
         string& res = responses.front();
+        std::cout<<"["<<socket<<"]Sending response: "<<responses.front()<<std::endl<<std::flush;
         int bytes_sent = send(socket,res.c_str(), res.size(),MSG_DONTWAIT);
-
+        
         if(bytes_sent < res.size() && bytes_sent > 0) // erase sent fragment from response and keep it in the Q
             res.erase(0,bytes_sent);
         else // Whole response was sent - pop it from Q
             responses.pop_front();
     }
-
+        
     // Return true if read from socket won't block
     // Server needs to call select() for this to work
     bool isReadReady()
@@ -147,7 +149,7 @@ class Connection
         read_fdset = nullptr;
         write_fdset = nullptr;
         requests.clear();
-        responses.clear();
+        responses.clear(); 
     }
 };
 
