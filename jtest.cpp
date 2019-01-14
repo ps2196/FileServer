a1cf2785d2c8c9fce32706a3e6d907373a680ed9
Piotrek <slysz.piotrek@gmail.com>
2019-01-14 23:25:14 +0100

    base64 encode/decode example

--- jtest.cpp
+++ jtest.cpp
@@ -1,27 +1,90 @@
 #include "utils/json.hpp"
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <string.h>
 #include "auth_strategy/authstrategy.hpp"
-#include <boost/filesystem.hpp>
+//#include <boost/filesystem.hpp>
 #include <fstream>
 #include <vector>
 #include <list>
+#include "utils/cppcodec/base64_rfc4648.hpp"
 
 using json = nlohmann::json;
 using string = std::string;
-namespace FS = boost::filesystem;
+//namespace FS = boost::filesystem;
 using namespace std;
+
+void fencode(const string& sfile, const string& encfile)
+{
+    using base64 = cppcodec::base64_rfc4648;
+    ifstream f(sfile, ifstream::binary);
+    ofstream of(encfile, ios::out|ios::binary);
+    char buf[900];
+    char encbuf[2000];
+    
+    cout<<"Encoding ..."<<endl;
+    while( true )
+    {
+        f.read(buf, 900);
+        int rval = f.gcount();
+        cout<<"Bytes read = "<< rval <<endl;    
+        int enclen = base64::encode(encbuf, 2000, buf, rval);
+        cout<<"Encoded len = "<< enclen<<endl;
+        of.write(encbuf, enclen);
+        if(!f)
+            break;
+    }
+    f.close();
+    of.close();
+}
+
+
+
+void fdecode(const string& encfile, const string& rfile)
+{
+    using base64 = cppcodec::base64_rfc4648;
+    ifstream f(encfile, ifstream::binary);
+    ofstream of(rfile, ios::out|ios::binary);
+    char buf[1200];
+    char decbuf[900];
+    
+    cout<<"Encoding ..."<<endl;
+    while( true )
+    {
+        f.read(buf, 1200);
+        int rval = f.gcount();
+        cout<<"Bytes read = "<< rval <<endl;    
+        int declen = base64::decode(decbuf, 900, buf, rval);
+        cout<<"Encoded len = "<< declen<<endl;
+        of.write(decbuf, declen);
+        if(!f)
+            break;
+    }
+    f.close();
+    of.close();
+}
+
 int main()
 {   
+    using base64 = cppcodec::base64_rfc4648;
+    // int enclen = base64::encode(encbuf, 3000, buf.c_str(), buf.size());
+    // encbuf[enclen] = '\0';
+    // printf("%s", encbuf);
+    fencode("onet.html", "onetb64");
+    fdecode("onetb64","onet_decoded.html");
 
-    list<string> l;
-    string s1 = "Piotrek";
-    l.push_back(s1);
-    cout<<"S1.1 = "<< l.front() <<endl;
-    l.front().erase(0,3);
-    cout<<"S1.2 = "<< l.front() <<endl;
-    return 0;
+    //std::vector<uint8_t> encoded = base64::encode(char* encoded_result, size_t encoded_buffer_size, const [uint8_t|char]* binary, size_t binary_size);
+
+
+
+    // list<string> l;
+    // string s1 = "Piotrek";
+    // l.push_back(s1);
+    // cout<<"S1.1 = "<< l.front() <<endl;
+    // l.front().erase(0,3);
+    // cout<<"S1.2 = "<< l.front() <<endl;
+    // return 0;
 
     // std::ofstream f("/TINRepo/server/Hello.txt");
     // if(f.good())
